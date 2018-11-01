c971f4454540133675beb59b481cfa339129055e
Jean-Pierre Rupp <xenog@protonmail.com>
2018-11-01 11:10:46 +0000

    Fix log entry for ping time

--- src/Network/Haskoin/Node/Manager.hs
+++ src/Network/Haskoin/Node/Manager.hs
@@ -191,8 +191,8 @@ managerMessage (ManagerPeerMessage p msg@(MPong (Pong n))) = do
         Just d -> do
             let ms = fromRational . toRational $ d * 1000 :: Double
             $(logDebugS) "Manager" $
-                "Ping " <> cs (show n) <> " to " <> s <> " took " <>
-                cs (show ms)
+                "Ping roundtrip to " <> s <> " took " <>
+                cs (show ms) <> " ms"
 managerMessage (ManagerPeerMessage p (MPing (Ping n))) = do
     b <- asks onlinePeers
     s <- atomically $ peerString b p
